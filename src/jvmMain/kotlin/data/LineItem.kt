96444cbe32eab98ab5f9336f64c67383a3c501df
Paul Ellis <paulellis41@u.boisestate.edu>
2023-02-15 15:16:19 -0700

    [#62] line item data class implementation (#63)

---
+++ src/jvmMain/kotlin/data/LineItem.kt
@@ -0,0 +1,56 @@
+package data
+
+/**
+ * An Enum used in differentiating between card types, Reimbursements, and Travel in the Expense Workbook
+ *
+ * AH, PH, ME, JL - Different Cards Used for Purchases
+ *
+ * TRV - Any travel Expenses that were paid for with something other than one of the cards
+ *
+ * RMB - Any reimbursements paid for services, or material purchases
+ */
+enum class CardType {
+    AH,
+    PH,
+    ME,
+    JL,
+    TRV,
+    RMB
+}
+
+/**
+ * An Enum used in differentiating between purchasing types
+ * We will need to separate material purchases, travel, and services for tax reasons
+ *
+ * PURCHASE is any materials paid for that can be taxed, if a line item is of this type,
+ *          any NonTaxable totals is Shipping and Handling
+ *
+ * TRAVEL is any travel expenses (Car Rentals, Hotels, etc.)
+ *
+ * SERVICE is any services paid for (Welding, Sponsor Fee, etc.) - Non Taxable
+ */
+enum class PurchaseType{
+    PURCHASE, TRAVEL, SERVICE
+}
+
+/**
+ * This data class will be a line item read from the Expense Report to be used in the Team Expense Breakdown
+ * All fields are val's, so they cannot be changed once the LineItem is created.
+ *
+ * totalTaxable - Double value that is the total amount of the materials minus shipping and handling - cannot be null but can be 0
+ *
+ * totalNonTaxable - Double value of nontaxable totals this is shipping and handling, services, etc. - cannot be null but can be 0
+ *
+ * description - this is a string value that describes what the purchase was for
+ *
+ * date - this is a string containing the date of the purchase
+ *
+ * vendor - this is a string value of the vendor of the purchase (who sold the items purchased)
+ *
+ * cardType - this is an enum with different types of cards used in the Expense Workbook
+ *
+ * purchaseType - this is an enum that will allow us to separate Travel, services, and material purchases
+ */
+data class LineItem(val totalTaxable: Double, val totalNonTaxable: Double, val description: String,
+                    val date: String, val vendor: String, val cardType: CardType, val purchaseType: PurchaseType){
+}
---
+++ src/jvmTest/kotlin/DataClassTests.kt
@@ -0,0 +1,42 @@
+import data.CardType
+import data.LineItem
+import data.PurchaseType
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.DefaultAsserter.assertEquals
+import kotlin.test.DefaultAsserter.assertTrue
+
+class DataClassTests {
+
+    private var item1: LineItem? = null
+    private var expectedToString = ""
+    @BeforeEach
+    fun initVars(){
+        item1 = LineItem(1.11, 2.22, "test","1/2/23","Amazon", CardType.AH, PurchaseType.PURCHASE)
+        expectedToString = "LineItem(totalTaxable=1.11, totalNonTaxable=2.22, description=test, date=1/2/23, vendor=Amazon, cardType=AH, purchaseType=PURCHASE)"
+    }
+    @Test
+    fun lineItem_constructorAttempted_LineItemCreated(){
+        assertEquals("Create Failed", expectedToString, item1.toString())
+    }
+
+    @Test
+    fun lineItem_CopyCalled_IdenticalLineItemCreated(){
+        var item2 = item1?.copy()
+        assertEquals("Copy Failed", expectedToString, item2.toString())
+    }
+
+    @Test
+    fun lineItemNonTaxable_ZeroValueGiven_LineItemCreationSuccessful(){
+        var item2 = LineItem(3.00, 0.00, "Purchase with No S+H", "2/15/2023", "Home Depot", CardType.JL, PurchaseType.PURCHASE)
+        var item2ExpectedString = "LineItem(totalTaxable=3.0, totalNonTaxable=0.0, description=Purchase with No S+H, date=2/15/2023, vendor=Home Depot, cardType=JL, purchaseType=PURCHASE)"
+        assertEquals("No NonTax LineItem Creation Failed", item2ExpectedString,item2.toString())
+    }
+
+    @Test
+    fun lineItemTaxableZero_ZeroValueGiven_LineItemCreationSuccessful(){
+        var item2 = LineItem(0.00, 3.00, "Purchase with No S+H", "2/15/2023", "Home Depot", CardType.JL, PurchaseType.PURCHASE)
+        var item2ExpectedString = "LineItem(totalTaxable=0.0, totalNonTaxable=3.0, description=Purchase with No S+H, date=2/15/2023, vendor=Home Depot, cardType=JL, purchaseType=PURCHASE)"
+        assertEquals("No NonTax LineItem Creation Failed", item2ExpectedString,item2.toString())
+    }
+}

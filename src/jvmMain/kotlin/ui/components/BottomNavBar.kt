d7b8c7c2ca7ad2aa9121f62bd7a34aeed56b5ccf
ddmetz <dametz125@gmail.com>
2023-02-23 16:50:06 -0700

    add Wizard composable and WizardScreen abstract class

--- src/jvmMain/kotlin/ui/components/BottomNavBar.kt
+++ src/jvmMain/kotlin/ui/components/BottomNavBar.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,11 +19,10 @@ import androidx.compose.ui.unit.dp
  * Bottom navigation bar containing a next button and back button
  * @param onClickNext Function to be called when next is clicked
  * @param onClickBack Function to be called when back is clicked
- * @param nextEnabled Function used to check if next should be enabled
- * @param validateOnNext Will only call onClickNext if this returns true
+ * @param nextEnabled True if next button should be enabled
+ * @param backEnabled True if back button should be enabled
  * @param nextButtonText Text displayed on next button
  * @param backButtonText Text displayed on back button
- * @param content The content that should be located above this bar
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,51 +30,36 @@ fun BottomNavBar(
     onClickNext: () -> Unit,
     onClickBack: (() -> Unit)? = null,
     nextEnabled: () -> Boolean = { true },
-    validateOnNext: () -> Boolean = { true },
+    backEnabled: () -> Boolean = { true },
     nextButtonText: String = "Next >",
     backButtonText: String = "< Back",
-    content: @Composable () -> Unit,
 ) {
-    var backEnabled = true
-    if (onClickBack == null) {
-        backEnabled = false
-    }
-    Scaffold(
-        bottomBar = {
-            BottomAppBar(
-                tonalElevation = 3.dp,
-                contentPadding = PaddingValues(12.dp),
-                modifier = Modifier.requiredHeight(64.dp),
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth().fillMaxHeight(),
-                    horizontalArrangement = Arrangement.End,
-                ) {
-                    OutlinedButton(
-                        onClick = {
-                            if (onClickBack != null) {
-                                onClickBack()
-                            }
-                        },
-                        enabled = backEnabled,
-                        modifier = Modifier.padding(end = 8.dp),
-                    ) {
-                        Text(backButtonText)
-                    }
-                    OutlinedButton(
-                        onClick = {
-                            if (validateOnNext()) {
-                                onClickNext()
-                            }
-                        },
-                        enabled = nextEnabled(),
-                    ) {
-                        Text(nextButtonText)
-                    }
-                }
-            }
-        },
+    BottomAppBar(
+        tonalElevation = 3.dp,
+        contentPadding = PaddingValues(12.dp),
+        modifier = Modifier.requiredHeight(64.dp),
     ) {
-        content()
+        Row(
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            OutlinedButton(
+                onClick = {
+                    if (onClickBack != null) {
+                        onClickBack()
+                    }
+                },
+                enabled = backEnabled(),
+                modifier = Modifier.padding(end = 8.dp),
+            ) {
+                Text(backButtonText)
+            }
+            OutlinedButton(
+                onClick = { onClickNext() },
+                enabled = nextEnabled(),
+            ) {
+                Text(nextButtonText)
+            }
+        }
     }
 }

a195c3bb7701f5730ad8a0e431bce86430800a21
BreeLatimer <breelatimer@u.boisestate.edu>
2023-02-25 16:47:03 -0700

    Added yet another required dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,7 @@ kotlin {
                 implementation("com.github.pjfanning:excel-streaming-reader:4.0.5")
                 implementation("org.slf4j:slf4j-api:2.0.6")
                 implementation("org.slf4j:slf4j-jdk14:2.0.0")
+                implementation("org.apache.logging.log4j:log4j-core:2.20.0")
             }
         }
         val jvmTest by getting {

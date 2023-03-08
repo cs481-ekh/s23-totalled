a19d0d8daf0704c22c15d8dac847413d7c5e6918
PaulEllis2001 <paulellis41@u.boisestate.edu>
2023-03-08 16:35:43 -0700

    Added tests and fixed some issues

--- src/jvmMain/kotlin/processing/SheetToTeamParser.kt
+++ src/jvmMain/kotlin/processing/SheetToTeamParser.kt
@@ -2,23 +2,33 @@ package processing
 
 import org.apache.poi.ss.usermodel.Sheet
 
-class SheetToTeamParser (private var sheetList: MutableList<Sheet>) {
-    val headingIndiciesMap = HashMap<String, Int>()
-    fun populateColumnHeadingMap(){
-        for(curSheet in sheetList){
-            //Here is where to do stuff
-            if(curSheet.firstRowNum <0){
-                continue //There is no data in the sheet if firstRowNum is negative
+class SheetToTeamParser(private var sheetList: MutableList<Sheet>) {
+    private val tempHeadingIndiciesMap = HashMap<String, Int>()
+
+    // Because each sheet may contain different indicies for headings, we will store the map for each sheet
+    val sheetToHeadingsMap = HashMap<Int, HashMap<String, Int>>()
+    fun populateColumnHeadingMap() {
+        for ((index, curSheet) in sheetList.withIndex()) {
+            if (curSheet.firstRowNum < 0) {
+                continue // There is no data in the sheet if firstRowNum is negative
             }
-            for((index, curCell) in curSheet.getRow(curSheet.firstRowNum).withIndex()){
-                when (curCell.stringCellValue){
-                    "senior design po" -> headingIndiciesMap["senior design po"] = index
-                    "Business Purpose" -> headingIndiciesMap["Business Purpose"] = index
-                    "Total Amount" -> headingIndiciesMap["Total Amount"] = index
-                    "Amount 2- Shipping and handling costs. Senior Design Only" ->
-                        headingIndiciesMap["Shipping and Handling"] = index
+            for (curCell in curSheet.getRow(curSheet.firstRowNum)) {
+                // This is the kotlin equivalent of a switch statement.
+                // I am trimming the string to exclude leading or trailing whitespace, be all lowercase
+                // and for amount 2 excluding everything after the hyphen as it is the only heading with a hyphen
+                when (curCell.stringCellValue.substringBefore('-').lowercase().trim()) {
+                    "senior design po" -> tempHeadingIndiciesMap["senior design po"] = curCell.columnIndex
+                    "business purpose" -> tempHeadingIndiciesMap["Business Purpose"] = curCell.columnIndex
+                    "total amount" -> tempHeadingIndiciesMap["Total Amount"] = curCell.columnIndex
+                    // There are technically multiple headings with "amount 2" in each sheet,
+                    // so we only want the first one
+                    "amount 2" -> { if (!tempHeadingIndiciesMap.containsKey("Shipping and Handling")) {
+                        tempHeadingIndiciesMap["Shipping and Handling"] = curCell.columnIndex } }
                 }
             }
+            sheetToHeadingsMap[index] = HashMap<String, Int>(tempHeadingIndiciesMap)
+            // Clear current sheets mappings before moving on to the next one
+            tempHeadingIndiciesMap.clear()
         }
     }
 }

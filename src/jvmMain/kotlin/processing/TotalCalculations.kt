274e0b69933c8401da575f5dff80be2c9cbd06ed
ChaseStauts <60838137+ChaseStauts@users.noreply.github.com>
2023-03-09 21:43:54 -0700

    Reverted accidental commit, ktlint changes

--- src/jvmMain/kotlin/processing/TotalCalculations.kt
+++ src/jvmMain/kotlin/processing/TotalCalculations.kt
@@ -1,34 +1,31 @@
-import org.apache.poi.ss.usermodel.*
-import org.apache.poi.xssf.usermodel.DefaultIndexedColorMap
+package processing
+
+import org.apache.poi.ss.usermodel.BorderStyle
+import org.apache.poi.ss.usermodel.CellStyle
+import org.apache.poi.ss.usermodel.DataFormat
+import org.apache.poi.ss.usermodel.FillPatternType
+import org.apache.poi.ss.usermodel.HorizontalAlignment
+import org.apache.poi.ss.usermodel.IndexedColors
+import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.xssf.usermodel.XSSFColor
-import org.apache.poi.xssf.usermodel.XSSFFont
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import java.util.concurrent.TimeUnit
 
-fun main(){
-    val str1 = "C:\\Users\\staut\\Downloads\\TEMPLATE (team) Expense Breakdown.xlsx"
-    val file = File(str1)
-    val fIP = FileInputStream(file)
-    //val wb = java.io.XSSFWorkbook(FileInputStream(File(str1)))
-    //val fIP: java.io.FileInputStream = java.io.FileInputStream(file)
-    val workbook = XSSFWorkbook(fIP)
-    formatUtilFun(workbook, 5, 6, 6)
-    FileOutputStream("C:\\Users\\staut\\Downloads\\TEMPLATE (team) Expense Breakdown.xlsx").use { outputStream -> workbook.write(outputStream) }
-}
+/*
+    Takes the information given by existing workbook
+    Calculates totals based on different types of
+    merchandise, shipping, service, and travel
+    whilst formatting output of cells
+ */
 
-fun formatUtilFun(wb: Workbook, topCell: Int, bottomCell: Int, merchCol: Int) {
-    //initialize worksheet
+fun totalCalculations(wb: Workbook, topCell: Int, bottomCell: Int, merchCol: Int) {
+    // initialize worksheet
     val sheet = wb.getSheetAt(0)
 
-    //initialize data format for cell for dollar double type
+    // initialize data format for cell for dollar double type
     val dataFormat: DataFormat = wb.createDataFormat()
     var dollarStyle: CellStyle = wb.createCellStyle()
     dollarStyle.dataFormat = dataFormat.getFormat("$#,#0.00")
 
-    //set cell color and border color
+    // set cell color and border color
     dollarStyle.fillPattern = FillPatternType.SOLID_FOREGROUND
     val fillRgb = byteArrayOf(221.toByte(), 235.toByte(), 247.toByte())
     val color = XSSFColor()
@@ -43,18 +40,18 @@ fun formatUtilFun(wb: Workbook, topCell: Int, bottomCell: Int, merchCol: Int) {
     dollarStyle.borderRight = BorderStyle.THIN
     dollarStyle.rightBorderColor = IndexedColors.CORNFLOWER_BLUE.index
 
-    //Create cells and their column reference values
+    // Create cells and their column reference values
     val totalRow = sheet.createRow(bottomCell + 2)
     val merchColLetter = merchCol.toChar() + 65
-    val shipColLetter = (merchCol+1).toChar() + 65
-    val servColLetter = (merchCol+2).toChar() + 65
-    val travelColLetter = (merchCol+3).toChar() + 65
+    val shipColLetter = (merchCol + 1).toChar() + 65
+    val servColLetter = (merchCol + 2).toChar() + 65
+    val travelColLetter = (merchCol + 3).toChar() + 65
     val merchCell = totalRow.createCell(6)
     val shipCell = totalRow.createCell(7)
     val servCell = totalRow.createCell(8)
     val travelCell = totalRow.createCell(9)
 
-    //Set cell formula for total cells
+    // Set cell formula for total cells
     merchCell.cellFormula = "SUM($merchColLetter$topCell:$merchColLetter$bottomCell)"
     merchCell.cellStyle = dollarStyle
     shipCell.cellFormula = "SUM($shipColLetter$topCell:$shipColLetter$bottomCell)"
@@ -64,15 +61,15 @@ fun formatUtilFun(wb: Workbook, topCell: Int, bottomCell: Int, merchCol: Int) {
     travelCell.cellFormula = "SUM($travelColLetter$topCell:$travelColLetter$bottomCell)"
     travelCell.cellStyle = dollarStyle
 
-    //Set cell formula calculations for taxes
+    // Set cell formula calculations for taxes
     val taxRow = sheet.createRow(bottomCell + 3)
     val taxCell = taxRow.createCell(6)
     val eval = wb.creationHelper.createFormulaEvaluator()
     var res = eval.evaluate(merchCell).formatAsString().toDouble()
-    taxCell.cellFormula = "${res}*0.06"
+    taxCell.cellFormula = "$res*0.06"
     taxCell.cellStyle = dollarStyle
 
-    //Create and set cell for total sum of costs
+    // Create and set cell for total sum of costs
     val totalChargesRow = sheet.createRow(bottomCell + 5)
     val totalChargesCell = totalChargesRow.createCell(6)
     res = eval.evaluate(merchCell).formatAsString().toDouble()
@@ -83,16 +80,17 @@ fun formatUtilFun(wb: Workbook, topCell: Int, bottomCell: Int, merchCol: Int) {
     totalChargesCell.cellFormula = "$res + $res1 + $res2 + $res3 + $res4"
     totalChargesCell.cellStyle = dollarStyle
 
-    //set display cells with formatting
-    //create font style
-    var fontHeight = 14.toShort()
-    val stringStyle: CellStyle = wb.createCellStyle()
+    // Set display cells with formatting
+    // Create font style
+    var fontHeight = 16.toShort()
+    var stringStyle: CellStyle = wb.createCellStyle()
     stringStyle.alignment = HorizontalAlignment.RIGHT
     val font = wb.createFont()
     font.bold = true
     font.fontHeightInPoints = fontHeight
     stringStyle.setFont(font)
-    //create cells with font type
+
+    // Create cells with font type
     val totalString = totalRow.createCell(5)
     totalString.cellStyle = stringStyle
     totalString.setCellValue("Totals:")
@@ -103,7 +101,7 @@ fun formatUtilFun(wb: Workbook, topCell: Int, bottomCell: Int, merchCol: Int) {
     chargesString.cellStyle = stringStyle
     chargesString.setCellValue("TOTAL CHARGES")
 
-    //format leftover cells
+    // Format leftover cells
     taxRow.createCell(7).cellStyle = dollarStyle
     taxRow.createCell(8).cellStyle = dollarStyle
     taxRow.createCell(9).cellStyle = dollarStyle

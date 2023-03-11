b10ef4138f89d45c41804dc3f6021552593d5c97
ddmetz <dametz125@gmail.com>
2023-03-11 15:40:51 -0700

    remove useless lines from something i was testing

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,10 +1,5 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -13,7 +8,6 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import kotlinx.coroutines.delay
 import ui.components.WindowTitleBar
 import ui.components.Wizard
 import ui.theme.AppTheme
@@ -27,7 +21,6 @@ fun main() = application {
     )
     val title = "Totalled"
     val icon = painterResource("totalled-icon.svg")
-    var isOpen by remember { mutableStateOf(true) }
 
     Window(
         onCloseRequest = ::exitApplication,
@@ -38,10 +31,6 @@ fun main() = application {
         state = windowState,
     ) {
         AppTheme() {
-            LaunchedEffect(Unit) {
-                delay(2000) // Do some heavy lifting
-                isOpen = false
-            }
             Column(
                 modifier = Modifier.clip(RoundedCornerShape(8.dp)),
             ) {

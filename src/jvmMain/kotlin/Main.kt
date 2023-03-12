d9f7dc45f3a83b92b3c15a2eb8c4f412c646517f
ddmetz <danielmetz@u.boisestate.edu>
2023-03-11 22:19:07 -0800

    listen for focus + other improvements

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,9 +1,19 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.delay
 import ui.components.WindowTitleBar
 import ui.components.Wizard
 import ui.theme.AppTheme
@@ -15,15 +25,27 @@ fun main() = application {
         width = 600.dp,
         height = 450.dp,
     )
+    val title = "Totalled"
+    val icon = painterResource("totalled-icon.svg")
+    var isOpen by remember { mutableStateOf(true) }
+
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Totalled",
+        icon = icon,
+        title = title,
         undecorated = true,
+        transparent = true,
         state = windowState,
     ) {
         AppTheme() {
-            Column {
-                WindowTitleBar(windowState)
+            LaunchedEffect(Unit) {
+                delay(2000) // Do some heavy lifting
+                isOpen = false
+            }
+            Column(
+                modifier = Modifier.clip(RoundedCornerShape(8.dp)),
+            ) {
+                WindowTitleBar(windowState, title, icon)
                 Wizard()
             }
         }
--- src/jvmMain/kotlin/ui/components/WindowTitleBar.kt
+++ src/jvmMain/kotlin/ui/components/WindowTitleBar.kt
@@ -18,53 +18,88 @@ import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowScope
 import androidx.compose.ui.window.WindowState
+import java.awt.event.WindowEvent
+import java.awt.event.WindowFocusListener
 import kotlin.system.exitProcess
 
 @Composable
-fun WindowScope.WindowTitleBar(windowState: WindowState) {
+fun WindowScope.WindowTitleBar(windowState: WindowState, title: String, icon: Painter) {
+    var isFocused by remember { mutableStateOf(true) }
+    // Listening for focus events.
+    DisposableEffect(Unit) {
+        window.addWindowFocusListener(object : WindowFocusListener {
+            override fun windowGainedFocus(e: WindowEvent) {
+                isFocused = true
+            }
+
+            override fun windowLostFocus(e: WindowEvent) {
+                isFocused = false
+            }
+        })
+        onDispose { }
+    }
     WindowDraggableArea {
         Row(
-            modifier = Modifier.fillMaxWidth().height(24.dp).background(MaterialTheme.colorScheme.surfaceColorAtElevation(0.dp)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(28.dp)
+                .background(MaterialTheme.colorScheme.background),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            // Left
+            // Left side
             Box(Modifier.weight(.5f)) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Image(
-                        painterResource("totalled-icon.svg"),
-                        modifier = Modifier.height(16.dp).padding(start = 4.dp).clip(RoundedCornerShape(2.dp)),
+                        icon,
+                        modifier = Modifier.height(16.dp).padding(start = 6.dp).clip(RoundedCornerShape(2.dp)),
                         contentDescription = "App Icon",
                     )
                     Text(
-                        "Totalled",
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.onBackground,
+                        title,
+                        style = MaterialTheme.typography.labelMedium,
+                        color = if (isFocused) {
+                            MaterialTheme.colorScheme.onBackground
+                        } else {
+                            MaterialTheme.colorScheme.outline
+                        },
                         modifier = Modifier.padding(start = 4.dp),
                     )
                 }
             }
-            // Right
+            // Right side
             Box(Modifier.weight(.5f)) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.End,
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    TitleBarButton({ windowState.isMinimized = true }, "horizontal_rule_black_24dp.svg")
-                    TitleBarButton({ exitProcess(0) }, "close_black_24dp.svg")
+                    TitleBarButton(
+                        { windowState.isMinimized = true },
+                        "horizontal_rule_black_24dp.svg",
+                        isFocused,
+                    )
+                    TitleBarButton(
+                        { exitProcess(0) },
+                        "close_black_24dp.svg",
+                        isFocused,
+                    )
                 }
             }
         }
@@ -72,7 +107,7 @@ fun WindowScope.WindowTitleBar(windowState: WindowState) {
 }
 
 @Composable
-fun TitleBarButton(onClick: () -> Unit, iconResourcePath: String) {
+fun TitleBarButton(onClick: () -> Unit, iconResourcePath: String, isFocused: Boolean) {
     val interactionSource = remember { MutableInteractionSource() }
     Box(
         contentAlignment = Alignment.Center,
@@ -83,8 +118,12 @@ fun TitleBarButton(onClick: () -> Unit, iconResourcePath: String) {
         Icon(
             painterResource(iconResourcePath),
             contentDescription = "Close",
-            tint = MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier.fillMaxHeight(.75f),
+            tint = if (isFocused) {
+                MaterialTheme.colorScheme.onBackground
+            } else {
+                MaterialTheme.colorScheme.outline
+            },
+            modifier = Modifier.fillMaxHeight(.65f),
         )
     }
 }

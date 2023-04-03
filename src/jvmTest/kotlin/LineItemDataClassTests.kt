fb1c8477729a65a585ef79dd16765ce43d0f3ff6
Paul Ellis <paulellis41@u.boisestate.edu>
2023-04-03 16:30:35 -0600

    [#118] Fixed LineItem SheetToTeamParser and all the tests. (#120)

--- src/jvmTest/kotlin/LineItemDataClassTests.kt
+++ src/jvmTest/kotlin/LineItemDataClassTests.kt
@@ -12,8 +12,8 @@ class LineItemDataClassTests {
 
     @BeforeEach
     fun initVars() {
-        item1 = LineItem(1.11, 2.22, "test", "1/2/23", "Amazon", CardType.AH, PurchaseType.PURCHASE)
-        expectedToString = "LineItem(totalTaxable=1.11, totalNonTaxable=2.22, description=test, date=1/2/23, vendor=Amazon, cardType=AH, purchaseType=PURCHASE)"
+        item1 = LineItem(1.11, 2.22, "test", "1/2/23", "Amazon", CardType.AH, PurchaseType.PURCHASE, "team-001")
+        expectedToString = "LineItem(totalTaxable=1.11, totalNonTaxable=2.22, description=test, date=1/2/23, vendor=Amazon, cardType=AH, purchaseType=PURCHASE, poNumber=team-001)"
     }
 
     @Test
@@ -29,15 +29,15 @@ class LineItemDataClassTests {
 
     @Test
     fun lineItemNonTaxable_ZeroValueGiven_LineItemCreationSuccessful() {
-        val item2 = LineItem(3.00, 0.00, "Purchase with No S+H", "2/15/2023", "Home Depot", CardType.JL, PurchaseType.PURCHASE)
-        val item2ExpectedString = "LineItem(totalTaxable=3.0, totalNonTaxable=0.0, description=Purchase with No S+H, date=2/15/2023, vendor=Home Depot, cardType=JL, purchaseType=PURCHASE)"
+        val item2 = LineItem(3.00, 0.00, "Purchase with No S+H", "2/15/2023", "Home Depot", CardType.JL, PurchaseType.PURCHASE, "team-001")
+        val item2ExpectedString = "LineItem(totalTaxable=3.0, totalNonTaxable=0.0, description=Purchase with No S+H, date=2/15/2023, vendor=Home Depot, cardType=JL, purchaseType=PURCHASE, poNumber=team-001)"
         assertEquals("No NonTax LineItem Creation Failed", item2ExpectedString, item2.toString())
     }
 
     @Test
     fun lineItemTaxableZero_ZeroValueGiven_LineItemCreationSuccessful() {
-        val item2 = LineItem(0.00, 3.00, "Purchase with No S+H", "2/15/2023", "Home Depot", CardType.JL, PurchaseType.PURCHASE)
-        val item2ExpectedString = "LineItem(totalTaxable=0.0, totalNonTaxable=3.0, description=Purchase with No S+H, date=2/15/2023, vendor=Home Depot, cardType=JL, purchaseType=PURCHASE)"
+        val item2 = LineItem(0.00, 3.00, "Purchase with No S+H", "2/15/2023", "Home Depot", CardType.JL, PurchaseType.PURCHASE, "team-001")
+        val item2ExpectedString = "LineItem(totalTaxable=0.0, totalNonTaxable=3.0, description=Purchase with No S+H, date=2/15/2023, vendor=Home Depot, cardType=JL, purchaseType=PURCHASE, poNumber=team-001)"
         assertEquals("No NonTax LineItem Creation Failed", item2ExpectedString, item2.toString())
     }
 }

de60a21f701212df874af544064851a63cd8fc6c
Paul Ellis <paulellis41@u.boisestate.edu>
2023-04-03 17:06:52 -0600

    [#104] Added SheetToTeamParser to generateOutput (#119)

--- src/jvmMain/kotlin/processing/GenerateOutput.kt
+++ src/jvmMain/kotlin/processing/GenerateOutput.kt
@@ -1,5 +1,7 @@
 package processing
 
+import org.apache.poi.ss.usermodel.Sheet
+
 /**
  * Placeholder, rename package/file/function if needed.
  */
@@ -16,6 +18,21 @@ fun generateOutput(
     println(outputDirPath)
     println(columnNamesPath)
 
+    //This should probably be changed and the expenseLogPath2 should have some sort of nullable value
+    //or change this to have it be a lateInit...
+    val fileParser = if (expenseLogPath2.trim().isEmpty())
+        FileInputParser(expenseLogPath1, null) 
+    else 
+        FileInputParser(expenseLogPath1, expenseLogPath2) 
+    val sheetList = fileParser.getAllSheets()
+
+
+    val teamParser = SheetToTeamParser(sheetList)
+    val teams = teamParser.processAndGetTeams()
+    //Teams is a hash map with the String key value being the team name and the Team being the team
+    //object for the specific Team. This value will be passed to the next step which should be the
+    //Team output writer
+
     Thread.sleep(1000)
-    throw Exception("Output generation not implemented")
+    throw Exception("Output writing not implemented, parsing has been implemented and ${teams.size} teams found")
 }

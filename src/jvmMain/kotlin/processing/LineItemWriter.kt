75ea3714140f2ae838e86d5d82b78d185969b6bf
Bree Latimer <77258021+BreeLatimer@users.noreply.github.com>
2023-04-04 14:58:01 -0600

    [#91] write class that uses export template to create team expense breakdown (#122)

---
+++ src/jvmMain/kotlin/processing/LineItemWriter.kt
@@ -0,0 +1,123 @@
+package processing
+
+import data.PurchaseType
+import data.Team
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * This function/class simply writes out the TeamExpenseBreakdown when given a Team object.
+ */
+fun lineItemWriter(givenTeam: Team, outputPath: Path, fileName: String): Int {
+    var lastLineWritten = 3; // setting it to 3 because we want first line written to be 4, which is really 5. See the for loop.
+
+    var templateFile = File("src/jvmMain/resources/TeamExpenseBreakdownTemplate.xlsx")
+
+    var outputFilePath = Paths.get(outputPath.toString(), fileName)
+
+    var outputFile = outputFilePath.toFile()
+
+    // Make sure that we clean up any existing file
+    if (outputFile.exists()) {
+        outputFile.delete()
+    }
+
+    // Copy the contents of templateFile to outputFile
+    Files.copy(templateFile.toPath(), outputFile.toPath())
+
+    var workbook = XSSFWorkbook(FileInputStream(outputFile))
+
+    // Fill in appropriate cells with the corresponding information
+    var sheet = workbook.getSheetAt(0)
+
+    // Set up the headers
+    // Write the Semester (A1)
+    writeToCell(sheet, 0, 0, getSemester(givenTeam.lineItemList[0].date) + " Capstone Design")
+
+    // Write Team Name (C2)
+    writeToCell(sheet, 1, 2, givenTeam.teamName)
+
+    // Write Project Name (F2)
+    writeToCell(sheet, 1, 5, "${givenTeam.teamName} Project")
+
+    // Write out the line items (Starting on row 4 (5 in excel))
+    for (lineItem in givenTeam.lineItemList) {
+        lastLineWritten++
+
+        // write out lineItem attributes
+        writeToCell(sheet, lastLineWritten, 0, lineItem.cardType.toString())
+        writeToCell(sheet, lastLineWritten, 1, lineItem.poNumber)
+        writeToCell(sheet, lastLineWritten, 2, lineItem.vendor)
+        writeToCell(sheet, lastLineWritten, 3, lineItem.date)
+        writeToCell(sheet, lastLineWritten, 4, (lineItem.totalNonTaxable + lineItem.totalTaxable).toString())
+        writeToCell(sheet, lastLineWritten, 5, lineItem.description)
+
+        when (lineItem.purchaseType) {
+            PurchaseType.PURCHASE -> {
+                writeToCell(sheet, lastLineWritten, 6, lineItem.totalTaxable.toString())
+                writeToCell(sheet, lastLineWritten, 7, lineItem.totalNonTaxable.toString())
+            }
+            PurchaseType.SERVICE -> writeToCell(sheet, lastLineWritten, 8, lineItem.totalNonTaxable.toString())
+            PurchaseType.TRAVEL -> writeToCell(sheet, lastLineWritten, 9, lineItem.totalNonTaxable.toString())
+        }
+    }
+
+    workbook.write(FileOutputStream(outputFile))
+    // Close things up
+    workbook.close()
+    return lastLineWritten
+}
+
+/**
+ * Helper function to make writing to cells easier
+ */
+fun writeToCell(sheet: Sheet, rowIdx: Int, colIdx: Int, value: String) {
+    val rowIterator = sheet.rowIterator()
+    while (rowIterator.hasNext()) {
+        val currRow = rowIterator.next()
+        val cellIterator = currRow.cellIterator()
+        while (cellIterator.hasNext()) {
+            val cell = cellIterator.next()
+            if (cell.rowIndex == rowIdx && cell.columnIndex == colIdx) {
+                cell.setCellValue(value)
+                return
+            }
+        }
+    }
+}
+
+/**
+ * Helper function to make finding the date easier and to tidy up the above code
+ */
+private fun getSemester(dateAsString: String): String {
+    var acceptableFormats = mutableListOf<SimpleDateFormat>()
+    lateinit var date: Date
+
+    acceptableFormats.add(SimpleDateFormat("M/d/yyyy"))
+    acceptableFormats.add(SimpleDateFormat("M/dd/yyyy"))
+    acceptableFormats.add(SimpleDateFormat("MM/dd/yyyy"))
+    acceptableFormats.add(SimpleDateFormat("M/d/yy"))
+    acceptableFormats.add(SimpleDateFormat("M/dd/yy"))
+    acceptableFormats.add(SimpleDateFormat("MM/d/yy"))
+
+    for (pattern in acceptableFormats) {
+        try {
+            date = pattern.parse(dateAsString)
+        } catch (ex: ParseException) { }
+    }
+
+    var thisCalendar = Calendar.getInstance()
+    thisCalendar.setTime(date)
+    var year = thisCalendar.get(Calendar.YEAR)
+
+    return "Fiscal Year $year"
+}
---
+++ src/jvmTest/kotlin/processingTests/LineItemWriterTests.kt
@@ -0,0 +1,101 @@
+package processingTests
+
+import data.CardType
+import data.LineItem
+import data.PurchaseType
+import data.Team
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.junit.jupiter.api.Test
+import processing.lineItemWriter
+import java.nio.file.Paths
+import kotlin.test.junit5.JUnit5Asserter.assertEquals
+
+class LineItemWriterTests {
+    // fake data yoinked from TeamDataClassTests.kt, thank you Paul!
+    private val item1 = LineItem(12.00, 0.00, "Sheet Metal", "1/2/2023", "Home Depot", CardType.AH, PurchaseType.PURCHASE, "team-001")
+    private val item2 = LineItem(0.00, 1000.00, "Sponsor Fee", "8/1/2022", "MBE", CardType.NONE, PurchaseType.SERVICE, "team-001")
+    private val item3 = LineItem(12.00, 3.50, "Piping", "1/3/23", "PipesUSA.Com", CardType.RMB, PurchaseType.PURCHASE, "team-001")
+    private val item4 = LineItem(0.00, 140.0, "Hotel Stay", "12/25/23", "Hilton Garden Inn", CardType.TRV, PurchaseType.TRAVEL, "team-001")
+    private val lineItemList = mutableListOf(item1, item2, item3, item4)
+    private val teamName = "Team1"
+
+    private lateinit var testTeam: Team
+
+    var outputPath = Paths.get("src/jvmTest/TestOutputFiles")
+    var outputFileName = "LineItemWriterTests.xlsx"
+    var lastLineWrittenTo: Int = 0
+
+    private fun init() {
+        testTeam = Team(teamName, lineItemList)
+        // Call LineItemWriter on testTeam
+        lastLineWrittenTo = lineItemWriter(testTeam, outputPath, outputFileName)
+    }
+
+    private fun sampleTeamObject_LineItemsWritten_ReturnsCorrectLastRowWrittenTo() {
+        assertEquals("Expected last line written to to be 7, but got $lastLineWrittenTo", 7, lastLineWrittenTo)
+    }
+
+    @Test
+    fun sampleTeamObject_LineItemsWritten_ExpectedValuesPopulateExcelSheet() {
+        init()
+
+        // Ensure cell values are populated as expected
+        // Open LineItemWriterTests.xlsx
+        val inputStream = outputPath.resolve(outputFileName).toFile().inputStream()
+        val workbook = XSSFWorkbook(inputStream)
+        val sheet = workbook.getSheetAt(0)
+
+        // Assert that the cell values are as expected
+
+        // header cells
+        AssertCellAtIndexMatchesValue(sheet, 0, 0, "Fiscal Year 2023 Capstone Design")
+        AssertCellAtIndexMatchesValue(sheet, 1, 2, "Team1")
+        AssertCellAtIndexMatchesValue(sheet, 1, 5, "Team1 Project")
+
+        // item1
+        AssertCellAtIndexMatchesValue(sheet, 4, 0, "AH")
+        AssertCellAtIndexMatchesValue(sheet, 4, 1, "team-001")
+        AssertCellAtIndexMatchesValue(sheet, 4, 2, "Home Depot")
+        AssertCellAtIndexMatchesValue(sheet, 4, 3, "1/2/2023")
+        AssertCellAtIndexMatchesValue(sheet, 4, 4, "12.0")
+        AssertCellAtIndexMatchesValue(sheet, 4, 5, "Sheet Metal")
+        AssertCellAtIndexMatchesValue(sheet, 4, 6, "12.0")
+        AssertCellAtIndexMatchesValue(sheet, 4, 7, "0.0")
+
+        // item2
+        AssertCellAtIndexMatchesValue(sheet, 5, 0, "NONE")
+        AssertCellAtIndexMatchesValue(sheet, 5, 1, "team-001")
+        AssertCellAtIndexMatchesValue(sheet, 5, 2, "MBE")
+        AssertCellAtIndexMatchesValue(sheet, 5, 3, "8/1/2022")
+        AssertCellAtIndexMatchesValue(sheet, 5, 4, "1000.0")
+        AssertCellAtIndexMatchesValue(sheet, 5, 5, "Sponsor Fee")
+        AssertCellAtIndexMatchesValue(sheet, 5, 8, "1000.0")
+
+        // item3
+        AssertCellAtIndexMatchesValue(sheet, 6, 0, "RMB")
+        AssertCellAtIndexMatchesValue(sheet, 6, 1, "team-001")
+        AssertCellAtIndexMatchesValue(sheet, 6, 2, "PipesUSA.Com")
+        AssertCellAtIndexMatchesValue(sheet, 6, 3, "1/3/23")
+        AssertCellAtIndexMatchesValue(sheet, 6, 4, "15.5")
+        AssertCellAtIndexMatchesValue(sheet, 6, 5, "Piping")
+        AssertCellAtIndexMatchesValue(sheet, 6, 6, "12.0")
+        AssertCellAtIndexMatchesValue(sheet, 6, 7, "3.5")
+
+        // item4
+        AssertCellAtIndexMatchesValue(sheet, 7, 0, "TRV")
+        AssertCellAtIndexMatchesValue(sheet, 7, 1, "team-001")
+        AssertCellAtIndexMatchesValue(sheet, 7, 2, "Hilton Garden Inn")
+        AssertCellAtIndexMatchesValue(sheet, 7, 3, "12/25/23")
+        AssertCellAtIndexMatchesValue(sheet, 7, 4, "140.0")
+        AssertCellAtIndexMatchesValue(sheet, 7, 5, "Hotel Stay")
+        AssertCellAtIndexMatchesValue(sheet, 7, 9, "140.0")
+
+        sampleTeamObject_LineItemsWritten_ReturnsCorrectLastRowWrittenTo()
+    }
+
+    fun AssertCellAtIndexMatchesValue(sheet: Sheet, rowIndx: Int, colIdx: Int, value: String) {
+        val cell = sheet.getRow(rowIndx).getCell(colIdx)
+        assertEquals("Expected $value, but got ${cell.stringCellValue} instead!", value, cell.stringCellValue)
+    }
+}

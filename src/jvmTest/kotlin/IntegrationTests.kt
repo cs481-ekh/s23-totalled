ba9cf4148e7a7e40443b61cda7aaa81c93c8050c
PaulEllis2001 <paul@pdellis.com>
2023-04-18 18:33:34 -0600

    Integration Test for single workbook + Project Book

--- src/jvmTest/kotlin/IntegrationTests.kt
+++ src/jvmTest/kotlin/IntegrationTests.kt
@@ -11,6 +11,8 @@ class IntegrationTests {
     private val path: String = "$dir/src/jvmTest/TestInputFiles/PrimaryWorkbook.xlsx"
     private val outputPath: String = "$dir/src/jvmTest/TestOutputFiles"
     private val outputFileNames = listOf("$outputPath/me 310 Team Expense Breakdown.xlsx", "$outputPath/vitro Team Expense Breakdown.xlsx")
+    private val projectBook = "$dir/src/jvmTest/TestInputFiles/ProjectBookSample2.xlsx"
+    private val invoiceRequest = "$dir/src/jvmTest/TestOutputFiles/InvoiceRequest.xlsx"
 
     @Test
     fun givenSingleWorkbook_GenerateOutputCalled_OutputContainsValidData() {
@@ -67,6 +69,98 @@ class IntegrationTests {
                     5 -> {
                         assertEquals("PH", row.getCell(0).stringCellValue)
                         assertEquals(64.09, row.getCell(6).numericCellValue)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun givenSingleWorkbookAndProjectBook_GenerateOutputCalled_OutputContainsValidData() {
+        generateOutput(path, "", projectBook, outputPath, "")
+
+        val firstWorkbook: Workbook = StreamingReader.builder()
+            .rowCacheSize(100)
+            .bufferSize(700)
+            .open(FileInputStream(File(outputFileNames[0])))
+
+        val secondWorkbook: Workbook = StreamingReader.builder()
+            .rowCacheSize(100)
+            .bufferSize(700)
+            .open(FileInputStream(File(outputFileNames[1])))
+
+        val requestWorkbook: Workbook = StreamingReader.builder()
+            .rowCacheSize(100)
+            .bufferSize(700)
+            .open(FileInputStream(File(invoiceRequest)))
+
+        for (sheet in firstWorkbook) {
+            for ((index, row) in sheet.withIndex()) {
+                when (index) {
+                    1 -> {
+                        assertEquals("TEAM:", row.getCell(1).stringCellValue)
+                        assertEquals("me 310", row.getCell(2).stringCellValue)
+                    }
+                    3 -> {
+                        assertEquals("Card", row.getCell(0).stringCellValue)
+                        assertEquals("Travel Total", row.getCell(9).stringCellValue)
+                    }
+                    4 -> {
+                        assertEquals("PH", row.getCell(0).stringCellValue)
+                        assertEquals(21.60, row.getCell(6).numericCellValue)
+                    }
+                    5 -> {
+                        assertEquals("PH", row.getCell(0).stringCellValue)
+                        assertEquals(83.83, row.getCell(6).numericCellValue)
+                    }
+                }
+            }
+        }
+
+        for (sheet in secondWorkbook) {
+            for ((index, row) in sheet.withIndex()) {
+                when (index) {
+                    1 -> {
+                        assertEquals("TEAM:", row.getCell(1).stringCellValue)
+                        assertEquals("vitro", row.getCell(2).stringCellValue)
+                    }
+                    3 -> {
+                        assertEquals("Card", row.getCell(0).stringCellValue)
+                        assertEquals("Travel Total", row.getCell(9).stringCellValue)
+                    }
+                    4 -> {
+                        assertEquals("PH", row.getCell(0).stringCellValue)
+                        assertEquals(17.79, row.getCell(6).numericCellValue)
+                    }
+                    5 -> {
+                        assertEquals("PH", row.getCell(0).stringCellValue)
+                        assertEquals(64.09, row.getCell(6).numericCellValue)
+                    }
+                }
+            }
+        }
+
+        for (sheet in requestWorkbook) {
+            for ((index, row) in sheet.withIndex()) {
+                when (index) {
+                    0 -> {
+                        assertEquals("Billing Date", row.getCell(1).stringCellValue)
+                        assertEquals("Bill To:", row.getCell(2).stringCellValue)
+                    }
+                    2 -> {
+                        assertEquals("FILL OUT", row.getCell(0).stringCellValue)
+                        assertEquals("JB laser", row.getCell(2).stringCellValue)
+                        assertEquals("ID", row.getCell(9).stringCellValue)
+                    }
+                    6 -> {
+                        assertEquals("FILL OUT", row.getCell(0).stringCellValue)
+                        assertEquals("Visioneering Space Corp", row.getCell(2).stringCellValue)
+                        assertEquals("ID", row.getCell(9).stringCellValue)
+                    }
+                    10 -> {
+                        assertEquals("FILL OUT", row.getCell(0).stringCellValue)
+                        assertEquals("Visioneering Space Corp", row.getCell(2).stringCellValue)
+                        assertEquals("ID", row.getCell(9).stringCellValue)
                     }
                 }
             }
